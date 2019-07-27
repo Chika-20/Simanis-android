f5e7047a54581b6a826eec03c6cf44b7ceffe4bb
Chika-20 <nurfasikha98@gmail.com>
2019-07-27 11:19:05 +0700

    logout

--- app/src/main/java/com/simanisandroid/simanis/DataPasienFragment.java
+++ app/src/main/java/com/simanisandroid/simanis/DataPasienFragment.java
@@ -27,6 +27,7 @@ public class DataPasienFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        getActivity().setTitle("Simanis Apps");
         // Inflate the layout for this fragment
 
         View v = inflater.inflate(R.layout.fragment_data_pasien, container, false);
--- app/src/main/java/com/simanisandroid/simanis/HomeActivity.java
+++ app/src/main/java/com/simanisandroid/simanis/HomeActivity.java
@@ -1,20 +1,29 @@
 package com.simanisandroid.simanis;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 public class HomeActivity extends AppCompatActivity {
+    public LoginPreference loginPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        loginPref = new LoginPreference(getApplicationContext());
+
         BottomNavigationView bottomNav = findViewById(R.id.navigation);
         FirebaseMessaging.getInstance().subscribeToTopic("android");
         getSupportFragmentManager().beginTransaction().replace(R.id.main_container,new DataPasienFragment()).commit();
@@ -37,6 +46,34 @@ public class HomeActivity extends AppCompatActivity {
         });
     }
 
+    private void signOut() {
+        //firebase sign out
+        FirebaseAuth.getInstance().signOut();
+//        loginPref.setLoginStatus(false);
+//        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+//        finish();
+    }
+
+    //method sign out dialog
+    private void signoutDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Sign Out")
+                .setMessage("Do you want to sign out ?")
+                .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        signOut();
+                        loginPref.setLoginStatus(false);
+                        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                        finish();
+                    }
+                })
+                .setNegativeButton("NO", null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+//        Toast.makeText(this, "Berhasil Logout", Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     //method create option menu
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -44,4 +81,13 @@ public class HomeActivity extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout:
+                signoutDialog();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
